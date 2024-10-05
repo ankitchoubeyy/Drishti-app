8d8e15f5327478b79bba07a259e7bdf94c3e72d5
ankitchoubeyy <imkitchoubey@gmail.com>
2024-10-05 16:17:12 +0530

    Navbar added

---
+++ src/app/components/buttons/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <>
+        <button className='bg-[#FF6500] px-8 py-3 rounded-lg hover:bg-[#1E3E62] font-bold'>Sign In</button>
+    </>
+  )
+}
+
+export default Signin
---
+++ src/app/components/buttons/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <>
+        <button className='bg-slate-950 px-8 py-3 rounded-lg hover:bg-[#1E3E62] mr-4 font-bold'>Sign Up</button>
+    </>
+  )
+}
+
+export default Signup
